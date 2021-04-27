da2b2048aae492b78646705ff2c424512992a2d0
Mile Janev <mile.janev@willowtreeapps.com>
2021-04-27 08:55:40 +0200

    Test

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Mile Janev'
-export const siteTitle = 'Mile test with Next.js Sample Website'
+const name = 'Your Name'
+export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi there, I am FE developer learning Next.js</p>
+        <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
